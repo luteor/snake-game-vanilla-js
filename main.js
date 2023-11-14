4bcd2d6afc4494aa0a71beed060ae187126f51cd
leo-turco <leo.turco.83@gmail.com>
2023-11-14 19:21:51 +0100

    feat: :sparkles: Implementing random food display

--- main.js
+++ main.js
@@ -1,2 +1,34 @@
 import "./reset.css";
 import "./style.css";
+
+const gridSize = 21;
+
+const playBoardElement = document.querySelector(".play-board");
+
+startGame();
+
+function startGame() {
+  displayFood();
+}
+
+function displayFood() {
+  const randomGridCoordinates = geRandomGridCoordinates(gridSize);
+
+  const foodElement = document.createElement("div");
+  foodElement.classList.add("play-board__food");
+  foodElement.style.gridArea = `${randomGridCoordinates[1]}/${randomGridCoordinates[0]}`;
+
+  playBoardElement.appendChild(foodElement);
+}
+
+function geRandomGridCoordinates(gridSize) {
+  const xPosition = Math.floor(Math.random() * gridSize) + 1;
+  console.log(xPosition);
+
+  const yPosition = Math.floor(Math.random() * gridSize) + 1;
+  console.log(yPosition);
+
+  const randomGridCoordinates = [xPosition, yPosition];
+
+  return randomGridCoordinates;
+}
